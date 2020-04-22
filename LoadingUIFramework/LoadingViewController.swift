42919c342773360979ad4222d4747f2e84d5fd58
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-22 18:35:53 -0400

    Added a method to display an optional message briefly before disappearing and documentation for the framework so far.

--- LoadingUIFramework/LoadingViewController.swift
+++ LoadingUIFramework/LoadingViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
+/// The ViewController that presents a loading screen GIF.
 public class LoadingViewController: UIViewController {
     
+    /// A loading view GIF object.
     let loadingGIF = IndeterminateLoadingView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    public override func presentLoadingGIF() {
+    /// This method sets up the loading screen GIF.
+    public func presentLoadingGIF() {
         
     }
     
-    
+    /// This method displays an optional message briefly before disappearing.
+    public func loadingGIFWillDisappear() {
+        
+    }
 
 }
