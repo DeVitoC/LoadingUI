3556cb3470a1d407d9b36406e8adabeec5079ac2
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-22 18:21:31 -0400

    Added LoadingViewController to LoadingUIFramework. added constant for the GIF and a method to present the GIF.

---
+++ LoadingUIFramework/LoadingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoadingViewController.swift
+//  LoadingUIFramework
+//
+//  Created by Christopher Devito on 4/22/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import UIKit
+
+public class LoadingViewController: UIViewController {
+    
+    let loadingGIF = IndeterminateLoadingView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    public override func presentLoadingGIF() {
+        
+    }
+    
+    
+
+}
