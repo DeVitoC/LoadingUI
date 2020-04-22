e71531528449f58e4536b211a5b86e4154c6a482
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-22 17:59:13 -0400

    Initial commit. Added LoadingUI Project, LoadingUIFramework, and added LoadingUIFramework to LoadingUI in

---
+++ LoadingUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUI
+//
+//  Created by Christopher Devito on 4/22/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoadingUIFramework/LoadingUIFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUIFramework.h
+//  LoadingUIFramework
+//
+//  Created by Christopher Devito on 4/22/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUIFramework.
+FOUNDATION_EXPORT double LoadingUIFrameworkVersionNumber;
+
+//! Project version string for LoadingUIFramework.
+FOUNDATION_EXPORT const unsigned char LoadingUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUIFramework/PublicHeader.h>
+
+
